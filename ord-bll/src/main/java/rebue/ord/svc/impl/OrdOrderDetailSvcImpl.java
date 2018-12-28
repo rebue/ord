4100260c11a8d2a336c8d7d46da9f7f5c1a0e3ff
zbz <nnzbz@qq.com>
2018-12-28 14:48:22 +0800

    * 修正计算首单空指针异常的BUG

--- ord-bll/src/main/java/rebue/ord/svc/impl/OrdOrderDetailSvcImpl.java
+++ ord-bll/src/main/java/rebue/ord/svc/impl/OrdOrderDetailSvcImpl.java
@@ -85,12 +85,12 @@ public class OrdOrderDetailSvcImpl extends BaseSvcImpl<java.lang.Long, OrdOrderD
 
     @Resource
     private SucOrgSvc         sucOrgSvc;
-    
+
     @Resource
-    private OrdTaskSvc ordTaskSvc;
-    
+    private OrdTaskSvc        ordTaskSvc;
+
     @Resource
-    private OrdOrderSvc         ordOrderSvc;
+    private OrdOrderSvc       ordOrderSvc;
 
     /**
      * 修改订单详情的退货情况(根据订单详情ID、已退货数量、旧的返现金总额，修改退货总数、返现金总额以及退货状态)
@@ -193,129 +193,128 @@ public class OrdOrderDetailSvcImpl extends BaseSvcImpl<java.lang.Long, OrdOrderD
         return _mapper.getOrderDetailForOneCommissonSlot(map);
     }
 
-	@Override
-	public List<DetailandBuyRelationRo> listBuyRelationByOrderId(final Long orderId) {
-		final List<DetailandBuyRelationRo> result = new ArrayList<>();
-		_log.info("根据orderId获取购买关系参数为： {}", orderId);
-		_log.info("先查询订单详情参数为： {}", orderId);
-		// 查询回来的订单详情列表
-		final List<OrdOrderDetailMo> detailList = _mapper.getDetailByOrderId(orderId);
-		_log.info("查询订单详情的返回值： {}", detailList);
-		// 根据订单详情列表中的id去获取购买关系
-		final OrdBuyRelationMo uPmo = new OrdBuyRelationMo();
-		final OrdBuyRelationMo dWmo = new OrdBuyRelationMo();
-		for (int i = 0; i < detailList.size(); i++) {
-			// 映射当前详情的所有字段
-			final DetailandBuyRelationRo item = new DetailandBuyRelationRo();
-			item.setCostPrice(detailList.get(i).getCostPrice());
-			item.setIsDeliver(detailList.get(i).getIsDelivered());
-			item.setId(detailList.get(i).getId());
-			item.setOrderId(detailList.get(i).getOrderId());
-			item.setProductId(detailList.get(i).getProductId());
-			item.setOnlineTitle(detailList.get(i).getOnlineTitle());
-			item.setSpecName(detailList.get(i).getSpecName());
-			item.setBuyCount(detailList.get(i).getBuyCount());
-			item.setBuyPrice(detailList.get(i).getBuyPrice());
-			item.setCashbackAmount(detailList.get(i).getCashbackAmount());
-			item.setBuyUnit(detailList.get(i).getBuyUnit());
-			item.setReturnCount(detailList.get(i).getReturnCount());
-			item.setReturnState(detailList.get(i).getReturnState());
-			item.setCashbackCommissionSlot(detailList.get(i).getCommissionSlot());
-			item.setCashbackCommissionState(detailList.get(i).getCommissionState());
-			item.setSubjectType(detailList.get(i).getSubjectType());
-			
-			_log.info("获取当前供应商参数为： {}", detailList.get(i).getSupplierId());
-			if(detailList.get(i).getSupplierId() != null) {
-				SucOrgRo sucOrgRo =sucOrgSvc.getById(detailList.get(i).getSupplierId());
-				_log.info("获取当前供应商结果为： {}", sucOrgRo.getRecord());
-				if(sucOrgRo !=null && sucOrgRo.getRecord() !=null &&sucOrgRo.getRecord().getName()!=null) {
-					item.setSupplierName(sucOrgRo.getRecord().getName());
-				}
-			}
-			uPmo.setUplineOrderDetailId(detailList.get(i).getId());
-			_log.info("当前下家关系的的参数为： {}", uPmo);
-			final List<OrdBuyRelationMo> Uplist = selfSvc.list(uPmo);
-			_log.info("查询下家关系的结果为： {}", Uplist);
-			for (int j = 0; j < Uplist.size(); j++) {
-				_log.info("开始获取下家信息开始--------------------------------------");
+    @Override
+    public List<DetailandBuyRelationRo> listBuyRelationByOrderId(final Long orderId) {
+        final List<DetailandBuyRelationRo> result = new ArrayList<>();
+        _log.info("根据orderId获取购买关系参数为： {}", orderId);
+        _log.info("先查询订单详情参数为： {}", orderId);
+        // 查询回来的订单详情列表
+        final List<OrdOrderDetailMo> detailList = _mapper.getDetailByOrderId(orderId);
+        _log.info("查询订单详情的返回值： {}", detailList);
+        // 根据订单详情列表中的id去获取购买关系
+        final OrdBuyRelationMo uPmo = new OrdBuyRelationMo();
+        final OrdBuyRelationMo dWmo = new OrdBuyRelationMo();
+        for (int i = 0; i < detailList.size(); i++) {
+            // 映射当前详情的所有字段
+            final DetailandBuyRelationRo item = new DetailandBuyRelationRo();
+            item.setCostPrice(detailList.get(i).getCostPrice());
+            item.setIsDeliver(detailList.get(i).getIsDelivered());
+            item.setId(detailList.get(i).getId());
+            item.setOrderId(detailList.get(i).getOrderId());
+            item.setProductId(detailList.get(i).getProductId());
+            item.setOnlineTitle(detailList.get(i).getOnlineTitle());
+            item.setSpecName(detailList.get(i).getSpecName());
+            item.setBuyCount(detailList.get(i).getBuyCount());
+            item.setBuyPrice(detailList.get(i).getBuyPrice());
+            item.setCashbackAmount(detailList.get(i).getCashbackAmount());
+            item.setBuyUnit(detailList.get(i).getBuyUnit());
+            item.setReturnCount(detailList.get(i).getReturnCount());
+            item.setReturnState(detailList.get(i).getReturnState());
+            item.setCashbackCommissionSlot(detailList.get(i).getCommissionSlot());
+            item.setCashbackCommissionState(detailList.get(i).getCommissionState());
+            item.setSubjectType(detailList.get(i).getSubjectType());
 
-				final Long dId = Uplist.get(j).getDownlineUserId();
-				final Long OrderId = Uplist.get(j).getDownlineOrderId();
-				//当前条购买关系的下家订单
-				_log.info("开始获取下家订单签收时间,订单id为： {}", OrderId);
-				OrdOrderMo ordOrderMo=ordOrderSvc.getById(OrderId);
-				_log.info("开始获取下家订单签收时间返回值： {}", ordOrderMo);
-				// 当前条购买关系的下家名字
-				_log.info("开始获取下家名字id为： {}", dId);
-				final SucUserMo dUserName = sucUserSvc.getById(dId);
-				_log.info("获取下家的结果为： {}", dUserName);
-				if (item.getDownlineUserName1() == null) {
-					_log.info("设置第一个下家名字： {}", dUserName);
-					item.setDownlineRelationSource1(Uplist.get(j).getRelationSource());
-					item.setDownlineIsSignIn1(Uplist.get(j).getIsSignIn());
-					if (dUserName != null) {
-						item.setDownlineUserName1(dUserName.getWxNickname());
-					}
-					//设置第一个下家的签收时间
-					_log.info("设置第一个下家签收时间： {}", ordOrderMo.getReceivedTime());
-					if(ordOrderMo !=null && ordOrderMo.getReceivedTime() !=null ) {
-						_log.info("设置第一个下家签收时间： {}", ordOrderMo.getReceivedTime());
-						item.setDownlineReceivedTime1(ordOrderMo.getReceivedTime());
-						item.setDownlineOrderCode1(ordOrderMo.getOrderCode());
-					}
-				} else {
-					_log.info("设置第二个下家名字： {}", dUserName);
-					item.setDownlineRelationSource2(Uplist.get(j).getRelationSource());
-					item.setDownlineIsSignIn2(Uplist.get(j).getIsSignIn());
-					if (dUserName != null) {
-						item.setDownlineUserName2(dUserName.getWxNickname());
-					}
-					//设置第二个下家的签收时间
-					_log.info("设置第二个下家签收时间： {}", ordOrderMo.getReceivedTime());
-					if(ordOrderMo !=null && ordOrderMo.getReceivedTime() !=null ) {
-						item.setDownlineReceivedTime2(ordOrderMo.getReceivedTime());
-						item.setDownlineOrderCode2(ordOrderMo.getOrderCode());
+            _log.info("获取当前供应商参数为： {}", detailList.get(i).getSupplierId());
+            if (detailList.get(i).getSupplierId() != null) {
+                final SucOrgRo sucOrgRo = sucOrgSvc.getById(detailList.get(i).getSupplierId());
+                _log.info("获取当前供应商结果为： {}", sucOrgRo.getRecord());
+                if (sucOrgRo != null && sucOrgRo.getRecord() != null && sucOrgRo.getRecord().getName() != null) {
+                    item.setSupplierName(sucOrgRo.getRecord().getName());
+                }
+            }
+            uPmo.setUplineOrderDetailId(detailList.get(i).getId());
+            _log.info("当前下家关系的的参数为： {}", uPmo);
+            final List<OrdBuyRelationMo> Uplist = selfSvc.list(uPmo);
+            _log.info("查询下家关系的结果为： {}", Uplist);
+            for (int j = 0; j < Uplist.size(); j++) {
+                _log.info("开始获取下家信息开始--------------------------------------");
 
-					}
-				}
-				_log.info("开始获取下家信息结束+++++++++++++++++++++++");
-			}
-			dWmo.setDownlineOrderDetailId(detailList.get(i).getId());
-			_log.info("当前订单详情上家关系的参数为： {}", dWmo);
-			// 已经获取到第一条订单详情的所有购买关系
-			final List<OrdBuyRelationMo> dwList = selfSvc.list(dWmo);
-			_log.info("当前订单详情上家关系的结果为： {}", dwList);
+                final Long dId = Uplist.get(j).getDownlineUserId();
+                final Long OrderId = Uplist.get(j).getDownlineOrderId();
+                // 当前条购买关系的下家订单
+                _log.info("开始获取下家订单签收时间,订单id为： {}", OrderId);
+                final OrdOrderMo ordOrderMo = ordOrderSvc.getById(OrderId);
+                _log.info("开始获取下家订单签收时间返回值： {}", ordOrderMo);
+                // 当前条购买关系的下家名字
+                _log.info("开始获取下家名字id为： {}", dId);
+                final SucUserMo dUserName = sucUserSvc.getById(dId);
+                _log.info("获取下家的结果为： {}", dUserName);
+                if (item.getDownlineUserName1() == null) {
+                    _log.info("设置第一个下家名字： {}", dUserName);
+                    item.setDownlineRelationSource1(Uplist.get(j).getRelationSource());
+                    item.setDownlineIsSignIn1(Uplist.get(j).getIsSignIn());
+                    if (dUserName != null) {
+                        item.setDownlineUserName1(dUserName.getWxNickname());
+                    }
+                    // 设置第一个下家的签收时间
+                    _log.info("设置第一个下家签收时间： {}", ordOrderMo.getReceivedTime());
+                    if (ordOrderMo != null && ordOrderMo.getReceivedTime() != null) {
+                        _log.info("设置第一个下家签收时间： {}", ordOrderMo.getReceivedTime());
+                        item.setDownlineReceivedTime1(ordOrderMo.getReceivedTime());
+                        item.setDownlineOrderCode1(ordOrderMo.getOrderCode());
+                    }
+                } else {
+                    _log.info("设置第二个下家名字： {}", dUserName);
+                    item.setDownlineRelationSource2(Uplist.get(j).getRelationSource());
+                    item.setDownlineIsSignIn2(Uplist.get(j).getIsSignIn());
+                    if (dUserName != null) {
+                        item.setDownlineUserName2(dUserName.getWxNickname());
+                    }
+                    // 设置第二个下家的签收时间
+                    _log.info("设置第二个下家签收时间： {}", ordOrderMo.getReceivedTime());
+                    if (ordOrderMo != null && ordOrderMo.getReceivedTime() != null) {
+                        item.setDownlineReceivedTime2(ordOrderMo.getReceivedTime());
+                        item.setDownlineOrderCode2(ordOrderMo.getOrderCode());
 
-			
-			for (int j = 0; j < dwList.size(); j++) {
-				_log.info("开始获取上家信息开始=======================");
+                    }
+                }
+                _log.info("开始获取下家信息结束+++++++++++++++++++++++");
+            }
+            dWmo.setDownlineOrderDetailId(detailList.get(i).getId());
+            _log.info("当前订单详情上家关系的参数为： {}", dWmo);
+            // 已经获取到第一条订单详情的所有购买关系
+            final List<OrdBuyRelationMo> dwList = selfSvc.list(dWmo);
+            _log.info("当前订单详情上家关系的结果为： {}", dwList);
 
-				_log.info("当前订单详情上家订单的参数为： {}", dwList.get(0).getUplineOrderId());
-				OrdOrderMo	ordOrderMo=ordOrderSvc.getById(dwList.get(0).getUplineOrderId());
-				_log.info("当前订单详情上家订单的返回值为： {}",ordOrderMo.getReceivedTime());
-				//设置上家的订单签收时间
-				if(ordOrderMo != null && ordOrderMo.getReceivedTime() !=null) {
-					item.setUplineReceivedTime(ordOrderMo.getReceivedTime());
-					item.setUplineOrderCode(ordOrderMo.getOrderCode());
-				}
-				
-				final Long uId = dwList.get(0).getUplineUserId();
-				// 当前条购买关系的上家名字
-				_log.info("开始获取上家名字id为： {}", uId);
-				final SucUserMo uUserName = sucUserSvc.getById(uId);
-				_log.info("获取上家的结果为： {}", uUserName);
-				item.setUplineRelationSource(dwList.get(0).getRelationSource());
-				item.setUplineIsSignIn(dwList.get(0).getIsSignIn());
-				if (uUserName != null) {
-					item.setUplineUserName(uUserName.getWxNickname());
-				}
-				_log.info("开始获取上家信息结束——————————————————————————");
+            for (int j = 0; j < dwList.size(); j++) {
+                _log.info("开始获取上家信息开始=======================");
 
-			}
-			result.add(item);
-		}
-		return result;
-	}
+                _log.info("当前订单详情上家订单的参数为： {}", dwList.get(0).getUplineOrderId());
+                final OrdOrderMo ordOrderMo = ordOrderSvc.getById(dwList.get(0).getUplineOrderId());
+                _log.info("当前订单详情上家订单的返回值为： {}", ordOrderMo.getReceivedTime());
+                // 设置上家的订单签收时间
+                if (ordOrderMo != null && ordOrderMo.getReceivedTime() != null) {
+                    item.setUplineReceivedTime(ordOrderMo.getReceivedTime());
+                    item.setUplineOrderCode(ordOrderMo.getOrderCode());
+                }
+
+                final Long uId = dwList.get(0).getUplineUserId();
+                // 当前条购买关系的上家名字
+                _log.info("开始获取上家名字id为： {}", uId);
+                final SucUserMo uUserName = sucUserSvc.getById(uId);
+                _log.info("获取上家的结果为： {}", uUserName);
+                item.setUplineRelationSource(dwList.get(0).getRelationSource());
+                item.setUplineIsSignIn(dwList.get(0).getIsSignIn());
+                if (uUserName != null) {
+                    item.setUplineUserName(uUserName.getWxNickname());
+                }
+                _log.info("开始获取上家信息结束——————————————————————————");
+
+            }
+            result.add(item);
+        }
+        return result;
+    }
 
     /**
      * 得到买家已下单指定上线规格商品的数量(以此来限制买家购买)
@@ -413,7 +412,7 @@ public class OrdOrderDetailSvcImpl extends BaseSvcImpl<java.lang.Long, OrdOrderD
             return;
         }
 
-        if (orderDetailMo.getPaySeq() == 1) {
+        if (orderDetailMo.getPaySeq() != null && orderDetailMo.getPaySeq() == 1) {
             _log.info("首单购买已经设置正确，无需修改");
             return;
         }
@@ -423,16 +422,16 @@ public class OrdOrderDetailSvcImpl extends BaseSvcImpl<java.lang.Long, OrdOrderD
 
         _log.debug("设置新的首单的支付顺序标志");
         _mapper.setFirstPaySeq(orderDetailMo.getId());
-        
+
         // TODO 查找是否有结算积分的子任务，如果有则添加结算首单积分的子任务
         OrdTaskMo ordTaskMo = new OrdTaskMo();
         ordTaskMo.setOrderId(orderDetailMo.getId().toString());
         ordTaskMo.setTaskType((byte) OrderTaskTypeDic.CALC_FIRST_BUY.getCode());
         _log.info("计算首单购买查询结算首单积分任务的参数为：{}", ordTaskMo);
-        OrdTaskMo taskMo = ordTaskSvc.getOne(ordTaskMo);
+        final OrdTaskMo taskMo = ordTaskSvc.getOne(ordTaskMo);
         _log.info("计算首单购买查询结算首单积分任务的参数为：{}", taskMo);
         if (taskMo == null) {
-        	_log.debug("添加计算首单的任务: orderDetail-{}", orderDetailMo);
+            _log.debug("添加计算首单的任务: orderDetail-{}", orderDetailMo);
             _log.debug("设置计算首单任务的执行时间为1分钟后执行");
             final Calendar calendar = Calendar.getInstance();
             calendar.setTime(new Date());
@@ -448,6 +447,6 @@ public class OrdOrderDetailSvcImpl extends BaseSvcImpl<java.lang.Long, OrdOrderD
             _log.debug("添加计算首单任务的参数为：{}", ordTaskMo);
             // 添加取消订单任务
             ordTaskSvc.add(ordTaskMo);
-		}
+        }
     }
 }
