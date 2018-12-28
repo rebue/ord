3c9dd99d399672aa113431c892adef9e2278d6cb
zbz <nnzbz@qq.com>
2018-12-28 22:19:59 +0800

    * 修正模板

--- ord-svr/src/main/java/rebue/ord/ctrl/OrdGoodsBuyRelationCtrl.java
+++ ord-svr/src/main/java/rebue/ord/ctrl/OrdGoodsBuyRelationCtrl.java
@@ -1,9 +1,9 @@
 package rebue.ord.ctrl;
 
-import com.github.pagehelper.PageInfo;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.pagehelper.PageInfo;
+
 import rebue.ord.mo.OrdGoodsBuyRelationMo;
 import rebue.ord.svc.OrdGoodsBuyRelationSvc;
 import rebue.robotech.dic.ResultDic;
@@ -30,7 +33,7 @@ public class OrdGoodsBuyRelationCtrl {
     /**
      * @mbg.generated 自动生成，如需修改，请删除本行
      */
-    private static final Logger _log = LoggerFactory.getLogger(OrdGoodsBuyRelationCtrl.class);
+    private static final Logger    _log             = LoggerFactory.getLogger(OrdGoodsBuyRelationCtrl.class);
 
     /**
      * @mbg.generated 自动生成，如需修改，请删除本行
@@ -43,40 +46,39 @@ public class OrdGoodsBuyRelationCtrl {
      *
      * @mbg.generated 自动生成，如需修改，请删除本行
      */
-    private String _uniqueFilesName = "某字段内容";
+    private final String           _uniqueFilesName = "某字段内容";
 
     /**
      * 添加用户商品购买关系
      */
     @PostMapping("/ord/goodsbuyrelation")
-    Ro add(@RequestBody OrdGoodsBuyRelationMo mo) throws Exception {
+    Ro add(@RequestBody final OrdGoodsBuyRelationMo mo) throws Exception {
         _log.info("add OrdGoodsBuyRelationMo: {}", mo);
-        Ro ro = new Ro();
+        final Ro ro = new Ro();
         try {
-            int result = svc.addEx(mo);
+            final int result = svc.addEx(mo);
             if (result == 1) {
-                String msg = "添加成功";
+                final String msg = "添加成功";
                 _log.info("{}: mo-{}", msg, mo);
                 ro.setMsg(msg);
                 ro.setResult(ResultDic.SUCCESS);
                 return ro;
             } else {
-                String msg = "添加失败";
+                final String msg = "添加失败";
                 _log.error("{}: mo-{}", msg, mo);
                 ro.setMsg(msg);
                 ro.setResult(ResultDic.FAIL);
                 return ro;
             }
-        } catch (DuplicateKeyException e) {
-            String msg = "添加失败，" + _uniqueFilesName + "已存在，不允许出现重复";
-            _log.error("{}: mo-{}", msg, mo);
+        } catch (final DuplicateKeyException e) {
+            final String msg = "添加失败，" + _uniqueFilesName + "已存在，不允许出现重复";
+            _log.error(msg + ": mo-" + mo, e);
             ro.setMsg(msg);
             ro.setResult(ResultDic.FAIL);
             return ro;
-        } catch (RuntimeException e) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String msg = "修改失败，出现运行时异常(" + sdf.format(new Date()) + ")";
-            _log.error("{}: mo-{}", msg, mo);
+        } catch (final RuntimeException e) {
+            final String msg = "添加失败，出现运行时异常";
+            _log.error(msg + ": mo-" + mo, e);
             ro.setMsg(msg);
             ro.setResult(ResultDic.FAIL);
             return ro;
@@ -89,33 +91,32 @@ public class OrdGoodsBuyRelationCtrl {
      * @mbg.generated 自动生成，如需修改，请删除本行
      */
     @PutMapping("/ord/goodsbuyrelation")
-    Ro modify(@RequestBody OrdGoodsBuyRelationMo mo) throws Exception {
+    Ro modify(@RequestBody final OrdGoodsBuyRelationMo mo) throws Exception {
         _log.info("modify OrdGoodsBuyRelationMo: {}", mo);
-        Ro ro = new Ro();
+        final Ro ro = new Ro();
         try {
             if (svc.modify(mo) == 1) {
-                String msg = "修改成功";
+                final String msg = "修改成功";
                 _log.info("{}: mo-{}", msg, mo);
                 ro.setMsg(msg);
                 ro.setResult(ResultDic.SUCCESS);
                 return ro;
             } else {
-                String msg = "修改失败";
+                final String msg = "修改失败";
                 _log.error("{}: mo-{}", msg, mo);
                 ro.setMsg(msg);
                 ro.setResult(ResultDic.FAIL);
                 return ro;
             }
-        } catch (DuplicateKeyException e) {
-            String msg = "修改失败，" + _uniqueFilesName + "已存在，不允许出现重复";
-            _log.error("{}: mo-{}", msg, mo);
+        } catch (final DuplicateKeyException e) {
+            final String msg = "修改失败，" + _uniqueFilesName + "已存在，不允许出现重复";
+            _log.error(msg + ": mo-" + mo, e);
             ro.setMsg(msg);
             ro.setResult(ResultDic.FAIL);
             return ro;
-        } catch (RuntimeException e) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String msg = "修改失败，出现运行时异常(" + sdf.format(new Date()) + ")";
-            _log.error("{}: mo-{}", msg, mo);
+        } catch (final RuntimeException e) {
+            final String msg = "修改失败，出现运行时异常";
+            _log.error(msg + ": mo-" + mo, e);
             ro.setMsg(msg);
             ro.setResult(ResultDic.FAIL);
             return ro;
@@ -128,18 +129,18 @@ public class OrdGoodsBuyRelationCtrl {
      * @mbg.generated 自动生成，如需修改，请删除本行
      */
     @DeleteMapping("/ord/goodsbuyrelation")
-    Ro del(@RequestParam("id") java.lang.Long id) {
+    Ro del(@RequestParam("id") final java.lang.Long id) {
         _log.info("del OrdGoodsBuyRelationMo by id: {}", id);
-        int result = svc.del(id);
-        Ro ro = new Ro();
+        final int result = svc.del(id);
+        final Ro ro = new Ro();
         if (result == 1) {
-            String msg = "删除成功";
+            final String msg = "删除成功";
             _log.info("{}: id-{}", msg, id);
             ro.setMsg(msg);
             ro.setResult(ResultDic.SUCCESS);
             return ro;
         } else {
-            String msg = "删除失败，找不到该记录";
+            final String msg = "删除失败，找不到该记录";
             _log.error("{}: id-{}", msg, id);
             ro.setMsg(msg);
             ro.setResult(ResultDic.FAIL);
@@ -153,18 +154,21 @@ public class OrdGoodsBuyRelationCtrl {
      * @mbg.generated 自动生成，如需修改，请删除本行
      */
     @GetMapping("/ord/goodsbuyrelation")
-    PageInfo<OrdGoodsBuyRelationMo> list(OrdGoodsBuyRelationMo mo, @RequestParam(value = "pageNum", required = false) Integer pageNum, @RequestParam(value = "pageSize", required = false) Integer pageSize) {
-        if (pageNum == null)
+    PageInfo<OrdGoodsBuyRelationMo> list(final OrdGoodsBuyRelationMo mo, @RequestParam(value = "pageNum", required = false) Integer pageNum,
+            @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        if (pageNum == null) {
             pageNum = 1;
-        if (pageSize == null)
+        }
+        if (pageSize == null) {
             pageSize = 5;
+        }
         _log.info("list OrdGoodsBuyRelationMo:" + mo + ", pageNum = " + pageNum + ", pageSize = " + pageSize);
         if (pageSize > 50) {
-            String msg = "pageSize不能大于50";
+            final String msg = "pageSize不能大于50";
             _log.error(msg);
             throw new IllegalArgumentException(msg);
         }
-        PageInfo<OrdGoodsBuyRelationMo> result = svc.list(mo, pageNum, pageSize);
+        final PageInfo<OrdGoodsBuyRelationMo> result = svc.list(mo, pageNum, pageSize);
         _log.info("result: " + result);
         return result;
     }
@@ -175,7 +179,7 @@ public class OrdGoodsBuyRelationCtrl {
      * @mbg.generated 自动生成，如需修改，请删除本行
      */
     @GetMapping("/ord/goodsbuyrelation/getbyid")
-    OrdGoodsBuyRelationMo getById(@RequestParam("id") java.lang.Long id) {
+    OrdGoodsBuyRelationMo getById(@RequestParam("id") final java.lang.Long id) {
         _log.info("get OrdGoodsBuyRelationMo by id: " + id);
         return svc.getById(id);
     }
